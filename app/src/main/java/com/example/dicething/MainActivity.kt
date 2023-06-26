176ac1aa1a6892010237177f10cbd80cd0507a2e
apenasliana <miresukali@gmail.com>
2023-06-26 17:02:58 -0300

    adding dice images

--- app/src/main/java/com/example/dicething/MainActivity.kt
+++ app/src/main/java/com/example/dicething/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.dicething
 
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.activity.ComponentActivity
 import androidx.compose.material3.Text
@@ -11,19 +12,28 @@ import androidx.compose.ui.tooling.preview.Preview
 import com.example.dicething.ui.theme.DicethingTheme
 
 class MainActivity : ComponentActivity() {
+
+    lateinit var diceImage : ImageView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        diceImage = findViewById(R.id.dice_image)
 
 
         val rollButton: Button = findViewById(R.id.roll_button)
         rollButton.setOnClickListener { rollDice() }
     }
     private fun rollDice() {
-        val randomInt = (1..100).random()
-        val resultText: TextView = findViewById(R.id.result_text)
-        resultText.text = randomInt.toString()
+        val randomInt = (1..6).random()
+        val drawableResource = when (randomInt) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        diceImage.setImageResource(drawableResource)
 //        Toast.makeText(this, "button clicked",
 //            Toast.LENGTH_SHORT).show()
     }
